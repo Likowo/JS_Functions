6e6c53fb9adeb8034f063ffecf72004c14f437cf
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-06 11:15:44 -0500

    Up date to JS Fxn

--- functions.js
+++ functions.js
@@ -43,7 +43,7 @@ console.log(maxOfThreethat(5,5,3))
 // Define a function, as a function declaration, isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise. // Parenthensis means () Parameters go in here and Curly brackets {} makes up the fxn body NB: The '.includes' Method works for both strings and Arrays
 console.log("this is the 3rd question below")
 const isCharAVowelthat  = (letter) => {
-    const vowels = "aeiou"
+    const vowels = "aeiouy"
     if(vowels.includes(letter)){
         return true
     }else {
@@ -75,14 +75,54 @@ const sumArraythat = (arrayOfNums) => {
     return sum
 }
 console.log(sumArraythat([2,5,9,1]))
-console.log(sumArraythat([1,4,6,8]))
-console.log(sumArraythat([10,20,5,10]))
+console.log(sumArraythat([1,3,9,11]))   
+console.log(sumArraythat([10,20,50,10])) 
 
 // Define a function, as a function declaration, multiplyArraythat takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+console.log("this is the 5th question below")
+const multiplyArray = (prodOfArray) => {
+    let product = prodOfArray[0]
+    for(i = 1;i<prodOfArray.length; i++){
+        product*= prodOfArray[i]
+    }
+    console.log(product)
+}
+  multiplyArray([3,2,2]) 
+// console.log("this is the 5th question below")
+// function multiplyArray([prodOfArray]){
+//     let product = prodOfArray[0]
+//     for(let i=1;i<prodOfArray.length; i++){
+//        product*= prodOfArray[i]
+//         console.log(product)
+//     }
+//     // return product   
+// }
+// console.log(multiplyArray([3,2,2]))
+
+//======================
+
 
 
 // Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
-// Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+// // Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+
+// const reverseStr = (str) => {
+//     let choppedWord = str.split("")
+//     let reversedWord = choppedWord.reverse()
+//       console.log(reversedWord)
+// }
+// reverseStr("window")
+
+
 // Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string.
 // Define a function, as a function declaration, stringsLongerThanthat takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
-
+// const  stringsLongerThan = (str,num) => {
+//     let longerThan = []
+//     str.forEach((word)) => {
+//         if(word.length > num){
+//           LongerThan.push(word)
+//         }
+//     }
+//     console.log(longerThan)
+// }
+// stringsLongerThan(["bradcool","Ben","Charlie","mary"],5)
